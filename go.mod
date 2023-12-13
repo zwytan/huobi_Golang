348f316316ea2b58d4fcca458cddaf96f9a7d38c
tanzhiwei <269839169@qq.com>
2023-12-13 10:37:28 +0800

    增加/v1/dw/withdraw/api/create client-order-id

--- go.mod
+++ go.mod
@@ -1,9 +1,14 @@
 module github.com/huobirdcenter/huobi_golang
 
-go 1.13
+go 1.20
 
 require (
 	github.com/gorilla/websocket v1.4.1
 	github.com/shopspring/decimal v1.2.0
 	go.uber.org/zap v1.15.0
+)
+
+require (
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
 )
--- pkg/model/wallet/createwithdrawrequest.go
+++ pkg/model/wallet/createwithdrawrequest.go
@@ -1,10 +1,11 @@
 package wallet
 
 type CreateWithdrawRequest struct {
-	Address  string `json:"address"`
-	Amount   string `json:"amount"`
-	Currency string `json:"currency"`
-	Fee      string `json:"fee"`
-	Chain    string `json:"chain,omitempty"`
-	AddrTag  string `json:"addr-tag,omitempty"`
+	Address       string `json:"address"`
+	Amount        string `json:"amount"`
+	Currency      string `json:"currency"`
+	Fee           string `json:"fee"`
+	Chain         string `json:"chain,omitempty"`
+	AddrTag       string `json:"addr-tag,omitempty"`
+	ClientOrderId string `json:"client-order-id,omitempty"`
 }
